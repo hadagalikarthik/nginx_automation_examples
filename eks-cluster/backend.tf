44016d747b0b57da4de9eb6154a74d13b4828923
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-03-28 20:09:39 +0530

    Removed unwanted code in backend.tf

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    # bucket         = ""       # Your S3 bucket name
     key            = "eks-cluster/terraform.tfstate"       # Path to state file
-    # region         = ""                   # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
     encrypt        = true                        
   }
--- nap/backend.tf
+++ nap/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    # bucket         = "" # Replace with your actual bucket name
     key            = "nap/terraform.tfstate"       # Path to state file
-    # region         = ""                    # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
     encrypt        = true                            # Encrypt state file at rest
   }
