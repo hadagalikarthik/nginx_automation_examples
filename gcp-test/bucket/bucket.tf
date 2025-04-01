c9df8060f137bda897737c43b536b816cc9840c4
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-01 14:09:35 +0530

    Deploy bucket and infra

--- gcp-test/bucket/bucket.tf
+++ gcp-test/bucket/bucket.tf
@@ -2,3 +2,47 @@ resource "google_storage_bucket" "bucket" {
   name     = var.GCP_BUCKET_NAME
   location = var.GCP_REGION
 }
+
+# # Firestore Collection for State Locking
+# resource "google_firestore_collection" "terraform_locks" {
+#   # count = local.firestore_exists ? 0 : 1
+#
+#   project  = var.GCP_PROJECT_ID
+#   name     = "terraform-lock-table"  # Firestore collection name
+# }
+#
+# Firestore Document for LockID (representing the lock)
+resource "google_firestore_document" "terraform_lock" {
+  # count = local.firestore_exists ? 0 : 1
+
+  project       = var.GCP_PROJECT_ID
+  database      = "(default)"  # Default Firestore database
+  collection    = "terraform-lock-table"
+  document_id   = "LockID"  # The LockID used for state locking
+
+  fields = {
+    "LockID" = "terraform-lock"  # Document content to represent the lock
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+#
+# resource "google_datastore_entity" "terraform_lock" {
+#   # count = local.datastore_exists ? 0 : 1
+#
+#   project     = var.GCP_PROJECT_ID
+#   kind        = "TerraformLock"  # Entity kind for the lock
+#   key {
+#     name = "LockID"  # The unique identifier for the lock
+#   }
+#
+#   properties = {
+#     "LockID" = "terraform-lock"
+#   }
+#
+#   lifecycle {
+#     prevent_destroy = true  # Prevent accidental deletion of the lock entity
+#   }
+# }
