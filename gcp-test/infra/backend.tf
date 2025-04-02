4168334044f588b37674e987ff88a9d4213aeeeb
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-02 12:55:11 +0530

    Deploy GCS and Infra

--- gcp-test/infra/backend.tf
+++ gcp-test/infra/backend.tf
@@ -3,6 +3,7 @@ terraform {
   backend "gcs" {
     # bucket         = var.GCP_BUCKET_NAME
     prefix         = "terraform/infra/terraform.tfstate"
+    project     = var.GCP_PROJECT_ID
     # credentials    = var.GOOGLE_CREDENTIALS
   }
 }
