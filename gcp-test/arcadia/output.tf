8a5f3657d4e63810f9f3c2b5c34879ec9ab135d4
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-04 23:43:32 +0530

    Deploy Infra, GKE and NAP

--- gcp-test/arcadia/output.tf
+++ gcp-test/arcadia/output.tf
@@ -1,5 +1,5 @@
 output "external_name" {
   description = "The external hostname of NGINX Ingress from NAP"
   value       = data.tfe_outputs.gke.values.kubernetes_api_server_url
-
+  sensitive = true
 }
