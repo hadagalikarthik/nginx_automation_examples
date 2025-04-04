edfda875bbe3e115d7d6a1666946a1fe1626a082
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-04 17:02:03 +0530

    Deploy Infra, GKE and NAP

--- gcp-test/nap/variables.tf
+++ gcp-test/nap/variables.tf
@@ -11,16 +11,16 @@ variable "nginx_pwd" {
   default     = "none"
 }
 
-# variable "workspace_path" {
-#   description = "The path to the workspace directory"
-#   type        = string
-# }
-#
-# variable "nginx_jwt" {
-#   description = "The JWT token for NGINX"
-#   type        = string
-#   sensitive   = true  # Mark as sensitive to avoid exposing it in logs
-# }
+variable "workspace_path" {
+  description = "The path to the workspace directory"
+  type        = string
+}
+
+variable "nginx_jwt" {
+  description = "The JWT token for NGINX"
+  type        = string
+  sensitive   = true  # Mark as sensitive to avoid exposing it in logs
+}
 
 variable "tf_cloud_organization" {
   type        = string
