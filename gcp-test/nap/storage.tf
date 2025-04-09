ce80f020daf06e284875bc8b67b13f95b16c6277
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-09 09:09:43 +0530

    removed storage block

--- gcp-test/nap/storage.tf
+++ gcp-test/nap/storage.tf
@@ -1,23 +1,23 @@
-resource "kubernetes_storage_class_v1" "gce_csi" {
-  metadata {
-    name = "standard-sc"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
-
-  # Provisioner for GCP Persistent Disks (GCE PD)
-  storage_provisioner = "kubernetes.io/gce-pd"
-
-  reclaim_policy = "Delete"  # Automatically delete volumes when they are no longer used.
-
-  parameters = {
-    type   = "pd-standard"   # This is the default disk type for GCP Persistent Disks.
-    fsType = "ext4"          # Filesystem type for the disk.
-  }
-
-  allow_volume_expansion = true
-
-  # Volume binding mode - control when the volume should be provisioned.
-  volume_binding_mode = "WaitForFirstConsumer"
-}
+# resource "kubernetes_storage_class_v1" "gce_csi" {
+#   metadata {
+#     name = "standard-sc"
+#     annotations = {
+#       "storageclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }
+#
+#   # Provisioner for GCP Persistent Disks (GCE PD)
+#   storage_provisioner = "kubernetes.io/gce-pd"
+#
+#   reclaim_policy = "Delete"  # Automatically delete volumes when they are no longer used.
+#
+#   parameters = {
+#     type   = "pd-standard"   # This is the default disk type for GCP Persistent Disks.
+#     fsType = "ext4"          # Filesystem type for the disk.
+#   }
+#
+#   allow_volume_expansion = true
+#
+#   # Volume binding mode - control when the volume should be provisioned.
+#   volume_binding_mode = "WaitForFirstConsumer"
+# }
